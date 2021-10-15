d9ce62b1192585aff6000b078e71f426ef183418
Jéssica Cordeiro <jessica.cordeiro121@gmail.com>
2021-10-15 08:33:49 -0300

    Lidando com eventos e Binds

--- ceep/src/components/FormularioCadastro/index.js
+++ ceep/src/components/FormularioCadastro/index.js
@@ -1,22 +1,35 @@
 import React, { Component } from "react";
 import "./style.css";
 
-export default function FormularioCadastro() {
-  return (
-    <form className="form-cadastro">
-      <input
-        type="text"
-        placeholder="Título"
-        className="form-cadastro_input"
-      />
-      <textarea
-        rows={15}
-        placeholder="Escreva sua nota..."
-        className="form-cadastro_input"
-      />
-      <button className="form-cadastro_input form-cadastro_submit">
-        Criar Nota
-      </button>
-    </form>
-  );
+export default class FormularioCadastro extends Component {
+
+  constructor() {
+    super();
+    this.titulo = "";
+  }
+
+  handleMudancaTitulo(evento) {
+    this.titulo = evento.target.value;
+  }
+  
+  render() {
+    return (
+      <form className="form-cadastro">
+        <input
+          type="text"
+          placeholder="Título"
+          className="form-cadastro_input"
+          onChange= {this.handleMudancaTitulo.bind(this)}
+        />
+        <textarea
+          rows={15}
+          placeholder="Escreva sua nota..."
+          className="form-cadastro_input"
+        />
+        <button className="form-cadastro_input form-cadastro_submit">
+          Criar Nota
+        </button>
+      </form>
+    );
+  }
 }
