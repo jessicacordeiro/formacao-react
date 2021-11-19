27702992d24839f8c12bafc32ca1ea05531a4ac8
Jéssica Cordeiro <jessica.alexandre-cordeiro@itau-unibanco.com.br>
2021-11-19 17:28:50 -0300

    Escutando eventos do click e capturado o valor

--- ceep-lista-de-tarefas/listaDeTarefas.js
+++ ceep-lista-de-tarefas/listaDeTarefas.js
@@ -1,7 +1,12 @@
-const novaTarefa = document.querySelector('[data-form-button]')
+const criarTarefa = (evento) => {
 
-novaTarefa.addEventListener('click', () => {
+    evento.preventDefault()
+    
     const input = document.querySelector('[data-form-input]')
     const valor = input.value
     console.log( valor )
-})
+    input.value = " "
+}
+const novaTarefa = document.querySelector('[data-form-button]')
+
+novaTarefa.addEventListener('click', criarTarefa)
