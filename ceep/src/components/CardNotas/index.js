7d5ffd3e5c5038b4eeb7549c707c361fe927b650
Jéssica Cordeiro <jessica.alexandre-cordeiro@itau-unibanco.com.br>
2021-11-22 23:45:20 -0300

    Importando SVGs como ícones da página

--- ceep/src/components/CardNotas/index.js
+++ ceep/src/components/CardNotas/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./style.css";
+import { ReactComponent as DeleteSVG } from "../../assets/img/delete.svg";
 
  class CardNotas extends Component {
      render() {
@@ -7,6 +8,7 @@ import "./style.css";
             <section className="card-nota">
                 <header className="card-nota_cabecalho">
                     <h3 className="card-nota_titulo">{this.props.title}</h3>
+                    <DeleteSVG />
                 </header>
                 <p className="card-nota_texto">{this.props.text}</p>
             </section>
